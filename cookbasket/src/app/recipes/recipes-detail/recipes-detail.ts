ea76ac4c9ab96ee8b847e0e2836b8be08cda7d48
Usama <usama18053lf@gmail.com>
2025-07-26 23:27:13 +0500

    all component created

---
+++ cookbasket/src/app/recipes/recipes-detail/recipes-detail.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-recipes-detail',
+  imports: [],
+  templateUrl: './recipes-detail.html',
+  styleUrl: './recipes-detail.css'
+})
+export class RecipesDetail {
+
+}
