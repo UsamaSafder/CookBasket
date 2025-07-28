36dec36e7dbac82afde64a4d15e3af45438bf2db
Usama <usama18053lf@gmail.com>
2025-07-28 22:19:56 +0500

    some changes made

--- cookbasket/src/app/recipes/recipes-list/recipes-list.ts
+++ cookbasket/src/app/recipes/recipes-list/recipes-list.ts
@@ -1,12 +1,16 @@
 import { Component } from '@angular/core';
 import { RecipesItem } from './recipes-item/recipes-item';
+import { Recipe } from '../recipe.model';
+import { NgFor } from '@angular/common';
 
 @Component({
   selector: 'app-recipes-list',
-  imports: [RecipesItem],
+  imports: [RecipesItem,NgFor],
   templateUrl: './recipes-list.html',
   styleUrl: './recipes-list.css'
 })
 export class RecipesList {
-
+  recipes:Recipe[]=[
+    new Recipe('Test recipe','this is a simply test','hello')
+  ]; 
 }
